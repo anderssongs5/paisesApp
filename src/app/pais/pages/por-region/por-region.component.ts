53c5409b4b544de3d0bb1071fcbdeab6c01a50b8
Andersson García Sotelo <agarcias@sura.com.co>
2021-12-09 21:37:58 -0500

    Adicionando nuevas funcionalidades

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+      button { margin-right: 5px; }
+    `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  getClaseCss(region: string): string {
+    return region === this.regionActiva ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
+  activarRegion(region: string) {
+    if(region === this.regionActiva) {
+      return;
+    }
+
+    this.paises = [];
+    this.regionActiva = region;
+    console.log(this.regionActiva);
+    this.paisService.buscarXRegion(region).subscribe({
+      next: (paises) => {
+        //debugger;
+        console.log('Next');
+        console.log(paises);
+        this.paises = paises;
+      },
+      error: (e) => {
+        //debugger;
+        console.log('Error');
+        console.error(e);
+        this.paises = [];
+      }
+    });
+  }
 }
